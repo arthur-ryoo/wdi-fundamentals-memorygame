02b6160c0cbfde25af995ab0b7b501d6e2ee282e
arthur-ryoo <jene0523@gmail.com>
2019-10-23 22:54:04 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,16 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-/* if (cardsInPlay.length === 2) {
-} else */ if (
-  cardsInPlay[0] === cardsInPlay[1]
-) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
