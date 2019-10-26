c957117b435f7f76ee01c7a0b38df38dac8f3ec1
arthur-ryoo <jene0523@gmail.com>
2019-10-26 13:24:09 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,20 +20,36 @@ var cards = [
     cardImage: "images/queen-of-diamonds.png"
   }
 ];
+
 var cardsInPlay = [];
-function checkForMatch() {
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!");
   } else {
     console.log("Sorry, try again.");
   }
-}
-function flipCard(cardId) {
+};
+
+var flipCard = function() {
+  var cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-}
-flipCard(0);
-flipCard(2);
-checkForMatch();
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+createBoard();
