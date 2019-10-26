5736d44778851d9950ac35a3a6325e57000422d3
arthur-ryoo <jene0523@gmail.com>
2019-10-26 13:48:43 -0700

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,9 +24,9 @@ var cards = [
 var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 };
 
